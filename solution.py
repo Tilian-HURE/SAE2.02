8afe2d7c2e1ec0325fc9ef62055bec618edb0b4d
Tilian HURÉ <58342016+Tilian-HURE@users.noreply.github.com>
2022-06-09 12:40:49 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,4 +1,12 @@
 # return string without spaces
-def erase(cc):
-
-    pass
+def erase(text):
+    """
+    Removes only single spaces from given string.
+    :param text : string to edit
+    :return : edited string
+    """
+    listedText = list(text)
+    for i, char in enumerate(listedText):
+        if (char == " " and listedText[max(i-1, 1)] != " " and listedText[min(i+1, len(listedText)-2)] != " "):
+            del listedText[i]
+    return "".join(char for char in listedText)

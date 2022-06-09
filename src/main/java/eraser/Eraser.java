9d2eda8e3663c55d4153a6cd3fdaced59a492ffe
Tilian <onsenfou>
2022-06-09 13:04:28 +0200

    Save phase 1

--- src/main/java/eraser/Eraser.java
+++ src/main/java/eraser/Eraser.java
@@ -1,7 +1,27 @@
 package eraser;
 
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class Eraser {
-    public static String erase(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
+
+    /**
+     * Removes only single spaces from given string.
+     * @param text : string to edit
+     * @return edited string
+     */
+    public static String erase(String text) {
+        char[] listedText = text.toCharArray();
+        int i = 0, textLen = listedText.length;
+        for (char chara: listedText) {
+            if (chara == ' ' || listedText[max(i-1, 1)] != ' ' || listedText[min(i+1, textLen-2)] != ' ') {
+                for (int j=i; j<textLen-1; j++) {
+                    listedText[j] = listedText[j+1];
+                }
+            }
+        }
+        return listedText.toString();
     }
+
 }
--- src/main/java/eraser/Main.java
+++ src/main/java/eraser/Main.java
@@ -1,8 +1,13 @@
 package eraser;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Eraser.erase(args[1]));
+        if (args.length == 0) {
+            System.out.println("TEST : " + Eraser.erase("test test  test    test"));
+        }
+        //System.out.println(Eraser.erase(args[1]));
     }
+
 }

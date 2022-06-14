4536aa25c6e3fa1d7ffff9e91462e075454d4539
Tilian HURÉ <tilian.contact@gmail.com>
2022-06-14 19:05:56 +0200

    Phase 2 : analyse efficacite21.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-import sys
-sys.path.append('analyse/solutions/') # import python files from path 'analyse/solutions'
-from efficacite21 import erase
+from solution import erase
 import codewars_test as test
 
 

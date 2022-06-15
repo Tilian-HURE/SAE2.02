759fdf120b32aee4210e08af5100d0f76f5c3a45
Tilian <onsenfou>
2022-06-15 12:48:02 +0200

    Save phase 2

--- analyse/solutions/efficacite21.py
+++ analyse/solutions/efficacite21.py
@@ -28,22 +28,18 @@ def erase(message: string):
         
         #   Who am I ?
 
-    compteur += 1
     if message == "": # Am I even real ? No...
         return message # So I just return myself : the void
     
         #   So I know that I'm not the void, am I space ?
-    compteur += 1
     if message[0] != ' ': # Surely not, you are discussing to the wrong person
         return message[0] + erase(message[1:]) # I enter the Valallah and the purge continues !
 
         #   Yeah it's me ! The famous and beautiful space char ;)
     else:
-        compteur += 1
         if len(message) > 1: # *you look behind* Am I the last one standing ?
 
                     # Hehe somebody is behind, that's nice
-                compteur += 1
                 if message[1] == ' ': # IT'S IT ! MY SPACE LOVER
                     return '  ' + erase(message[2:]) # WE ENTER THE VALHALLA TOGETHER ! And the purge continues (next episode)...
                 else: # Oh no, I've lost my space love ...
--- analyse/testAnalyse.py
+++ analyse/testAnalyse.py
@@ -8,17 +8,22 @@ import codewars_test as test
 import efficacite21
 @test.describe('\n\n===== TEST DU PROGRAMME efficacite21 =====')
 def sample_tests():
-    @test.it("Chaîne de taille 5")
+    @test.it("Chaîne de taille 1")
     def test1():
-        string = random_spaced_string(5)
+        string = random_spaced_string(1)
+        test.assert_equals(efficacite21.erase(string), efficacite21.erase(string))
+    
+    @test.it("Chaîne de taille 10")
+    def test2():
+        string = random_spaced_string(10)
         test.assert_equals(efficacite21.erase(string), efficacite21.erase(string))
 
-    @test.it("Chaîne de taille 15")
-    def test1():
-        string = random_spaced_string(15)
+    @test.it("Chaîne de taille 100")
+    def test3():
+        string = random_spaced_string(100)
         test.assert_equals(efficacite21.erase(string), efficacite21.erase(string))
 
-    @test.it("Chaîne de taille 30")
-    def test1():
-        string = random_spaced_string(30)
+    @test.it("Chaîne de taille 1000")
+    def test4():
+        string = random_spaced_string(4000)
         test.assert_equals(efficacite21.erase(string), efficacite21.erase(string))

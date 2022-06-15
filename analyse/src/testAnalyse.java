ac8a9c2ee2b6ff1440b4d014e2795ca2ba1af892
Tilian <onsenfou>
2022-06-15 15:44:02 +0200

    Save phase 2

---
+++ analyse/src/testAnalyse.java
@@ -0,0 +1,13 @@
+package analyse;
+
+
+import solutions.efficacite29;
+
+
+public class testAnalyse {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println(efficacite29.erase("test test"));
+    }
+
+}
